1c27317544d20c9419ac52c341768dc708988a7b
Minh Dao <daoducminh1997@gmail.com>
2020-12-16 12:45:26 +0700

    Update ml-1m

--- rec_sys/predict.py
+++ rec_sys/predict.py
@@ -1,6 +1,8 @@
+import random
 from rec_sys.helper import Dataset
 from rec_sys.recommenders import UserBasedCFRecommender, ContentBasedRecommender
 
+DATASET_1M = 'data/ml-1m'
 DATESET_SMALL = 'data/ml-latest-small'
 SCORES = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
 
@@ -21,14 +23,23 @@ def content_based_on_tags(dataset, movie_id):
 
 
 if __name__ == '__main__':
-    data = Dataset(DATESET_SMALL, 'csv', ',')
+    # data = Dataset(DATESET_SMALL, 'csv', ',')
+    data = Dataset(DATASET_1M, 'csv', ',')
+
     # Content based
-    print(content_based_on_tags(data, movie_id=246))
+    # print(content_based_on_tags(data, movie_id=246))
+
     # User based
+    # rate = {}
+    # ids = (1, 3, 6, 47, 50, 70, 101, 110, 151, 157, 163, 216, 223, 231, 235)
+    # sc = (4.0, 4.0, 4.0, 5.0, 5.0, 3.0, 5.0,
+    #       5.0, 4.0, 4.0, 4.5, 4.5, 3.5, 5.0, 4.5)
+    # for i in range(len(ids)):
+    #     rate[ids[i]] = sc[i]
+    # print(user_based_cf(data, rate, top_n=10, k_neighbor=50))
+
+    movies = list(data.load_df('movies').loc[:, 'movieId'])
     rate = {}
-    ids = (1, 3, 6, 47, 50, 70, 101, 110, 151, 157, 163, 216, 223, 231, 235)
-    sc = (4.0, 4.0, 4.0, 5.0, 5.0, 3.0, 5.0,
-          5.0, 4.0, 4.0, 4.5, 4.5, 3.5, 5.0, 4.5)
-    for i in range(len(ids)):
-        rate[ids[i]] = sc[i]
+    for i in range(50):
+        rate[movies[i]] = SCORES[random.randrange(0, len(SCORES), 1)]
     print(user_based_cf(data, rate, top_n=10, k_neighbor=50))
