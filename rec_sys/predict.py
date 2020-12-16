4917ee3f171b1a0d5c4382a3c553d9b632eed1d5
Minh Dao <daoducminh1997@gmail.com>
2020-12-16 12:33:44 +0700

    Finish basic func

--- rec_sys/predict.py
+++ rec_sys/predict.py
@@ -1,5 +1,3 @@
-import random
-
 from rec_sys.helper import Dataset
 from rec_sys.recommenders import UserBasedCFRecommender, ContentBasedRecommender
 
@@ -18,16 +16,19 @@ def user_based_cf(dataset, user_ratings, top_n=10, k_neighbor=20):
 
 def content_based_on_tags(dataset, movie_id):
     recommender = ContentBasedRecommender(dataset)
+    # return recommender.top_cos_genome_tag(movie_id, top_n=10)
     return recommender.top_cos_tag(movie_id, top_n=10)
 
 
 if __name__ == '__main__':
     data = Dataset(DATESET_SMALL, 'csv', ',')
     # Content based
-    # print(content_based_on_tags(data, movie_id=246))
+    print(content_based_on_tags(data, movie_id=246))
     # User based
-    movies = list(data.load_df('movies').loc[:, 'movieId'])
     rate = {}
-    for i in range(50):
-        rate[movies[i]] = SCORES[random.randrange(0, len(SCORES), 1)]
+    ids = (1, 3, 6, 47, 50, 70, 101, 110, 151, 157, 163, 216, 223, 231, 235)
+    sc = (4.0, 4.0, 4.0, 5.0, 5.0, 3.0, 5.0,
+          5.0, 4.0, 4.0, 4.5, 4.5, 3.5, 5.0, 4.5)
+    for i in range(len(ids)):
+        rate[ids[i]] = sc[i]
     print(user_based_cf(data, rate, top_n=10, k_neighbor=50))
--- rec_sys/recommenders.py
+++ rec_sys/recommenders.py
@@ -17,56 +17,65 @@ class UserBasedCFRecommender(Recommender):
         self.rating_df = self.dataset.load_df('ratings1').set_index('userId')
 
     def predict_movie_score(self, movie_id, top_df):
-        try:
-            a = top_df.loc[:, 'cos']
-            b = top_df.loc[:, str(movie_id)].mul(a)
-            sum_x = b.sum()
-            sum_y = a.sum()
-            return sum_x/sum_y
-        except:
-            return None
+        a = top_df.loc[:, 'cos']
+        b = top_df.loc[:, str(movie_id)].mul(a)
+        sum_x = b.sum()
+        sum_y = a.sum()
+        c = float(sum_x)/float(sum_y)
+        return c
 
     def top_movies(self, user_ratings, top_n=10, k_neighbor=50):
         user_id = self.rating_df.index.max() + 1
         id_series = self.rating_df.index
-        movie_list = list(id_series)
         ur_d = {
             user_id: user_ratings
         }
         ur_df = pd.DataFrame.from_dict(ur_d, orient='index')
-        # rating_df = self.rating_df[ur_df]
-        rating_df = pd.concat(
-            [ur_df, self.rating_df[ur_df.columns.intersection(
-                self.rating_df.columns)]]
-        ).sort_index()
+        ur_df.columns = ur_df.columns.map(str)
+
+        diff_df = self.rating_df[
+            self.rating_df.columns.difference(ur_df.columns)
+        ]
+
+        rating_df = self.rating_df[
+            ur_df.columns.intersection(self.rating_df.columns)
+        ]
+        ur_df = ur_df[
+            rating_df.columns.intersection(ur_df.columns)
+        ]
 
         avg = rating_df.mean(axis=1)
-        n_rating_df = rating_df.replace(0, np.NaN).subtract(avg, axis=0)
-        n1_rating_df = n_rating_df.replace(np.NaN, 0)
-        x_array = n1_rating_df.loc[user_id, :].to_numpy()
-        y_array = n1_rating_df.loc[:user_id - 1, :].to_numpy()
+        u_avg = ur_df.mean(axis=1)
 
-        t = cosine_similarity(x_array.reshape(1, -1), y_array)[0]
+        n_rating_df = rating_df.subtract(avg, axis=0)
+        n_ur_df = ur_df.subtract(u_avg, axis=0)
+        n_diff_df = diff_df.subtract(avg, axis=0)
+
+        x_array = n_ur_df.replace(np.NaN, 0).to_numpy()
+        y_array = n_rating_df.replace(np.NaN, 0).to_numpy()
+
+        t = cosine_similarity(x_array.reshape(1, -1), y_array)
         cosine_df = pd.DataFrame(
-            {'userId': id_series, 'cos': t}
+            {'userId': id_series, 'cos': t[0]}
         ).set_index('userId')
 
-        user_cos_df = merge_df_on_index(self.rating_df, cosine_df)
+        user_cos_df = merge_df_on_index(n_diff_df, cosine_df)
         user_cos_df = user_cos_df.sort_values('cos', ascending=False)
 
         top_df = user_cos_df.head(k_neighbor)
 
         rs = {}
         score = None
-        movies = list(rating_df)
-        for i in movies:
+        f_u_avg = float(u_avg)
+        for i in list(n_diff_df):
             score = self.predict_movie_score(i, top_df)
-            if score and not np.isnan(score):
-                rs[i] = score+t[-1]
+            if score:
+                rs[i] = score+f_u_avg
         if rs:
             a = pd.DataFrame.from_dict(
                 rs, orient='index'
             ).rename(columns={0: 'cos'})
+            a.index = a.index.map(int)
 
             b = merge_df_on_index(self.movie_df, a).sort_values(
                 'cos', ascending=False)
@@ -77,29 +86,34 @@ class UserBasedCFRecommender(Recommender):
     def mae(self, user_ratings, k_neighbor=50):
         user_id = self.rating_df.index.max() + 1
         id_series = self.rating_df.index
-        movie_list = list(id_series)
         ur_d = {
             user_id: user_ratings
         }
         ur_df = pd.DataFrame.from_dict(ur_d, orient='index')
-        # rating_df = self.rating_df[ur_df]
-        rating_df = pd.concat(
-            [ur_df, self.rating_df[ur_df.columns.intersection(
-                self.rating_df.columns)]]
-        ).sort_index()
+        ur_df.columns = ur_df.columns.map(str)
+
+        rating_df = self.rating_df[
+            ur_df.columns.intersection(self.rating_df.columns)
+        ]
+        ur_df = ur_df[
+            rating_df.columns.intersection(ur_df.columns)
+        ]
 
         avg = rating_df.mean(axis=1)
-        n_rating_df = rating_df.replace(0, np.NaN).subtract(avg, axis=0)
-        n1_rating_df = n_rating_df.replace(np.NaN, 0)
-        x_array = n1_rating_df.loc[user_id, :].to_numpy()
-        y_array = n1_rating_df.loc[:user_id - 1, :].to_numpy()
+        u_avg = ur_df.mean(axis=1)
 
-        t = cosine_similarity(x_array.reshape(1, -1), y_array)[0]
+        n_rating_df = rating_df.subtract(avg, axis=0)
+        n_ur_df = ur_df.subtract(u_avg, axis=0)
+
+        x_array = n_ur_df.replace(np.NaN, 0).to_numpy()
+        y_array = n_rating_df.replace(np.NaN, 0).to_numpy()
+
+        t = cosine_similarity(x_array.reshape(1, -1), y_array)
         cosine_df = pd.DataFrame(
-            {'userId': id_series, 'cos': t}
+            {'userId': id_series, 'cos': t[0]}
         ).set_index('userId')
 
-        user_cos_df = merge_df_on_index(self.rating_df, cosine_df)
+        user_cos_df = merge_df_on_index(n_rating_df, cosine_df)
         user_cos_df = user_cos_df.sort_values('cos', ascending=False)
 
         top_df = user_cos_df.head(k_neighbor)
@@ -107,14 +121,13 @@ class UserBasedCFRecommender(Recommender):
         p_score = None
         rs = 0
         count = 0
-        for i in list(rating_df):
-            if i in movie_list:
-                r_score = rating_df.loc[user_id, i]
-                if not pd.isna(r_score):
-                    p_score = self.predict_movie_score(i, top_df)
-                if p_score and not np.isnan(p_score):
-                    rs += abs(p_score - r_score)
-                    count += 1
+        f_u_avg = float(u_avg)
+        for i in list(n_rating_df):
+            r_score = ur_df.loc[user_id, i]
+            p_score = self.predict_movie_score(i, top_df)
+            if p_score and not np.isnan(p_score):
+                rs += abs(p_score + f_u_avg - r_score)
+                count += 1
         return rs / count
 
 
@@ -123,6 +136,7 @@ class ContentBasedRecommender(Recommender):
         super().__init__(dataset)
         self.genre_df = get_dummies_from_genre(self.movie_df)
         self.tag_df = self.dataset.load_df('tags1').set_index('movieId')
+        self.tag_genome_df = self.dataset.load_df('genome-scores1')
 
     def top_cos_tag(self, movie_id, top_n=10, tag_weight=1, genre_weight=1):
         if movie_id in self.tag_df.index:
@@ -150,4 +164,26 @@ class ContentBasedRecommender(Recommender):
             return []
 
     def top_cos_genome_tag(self, movie_id, top_n, tag_weight=1, genre_weight=1):
-        pass
+        if movie_id in self.tag_genome_df.index:
+            tg_df = merge_df_on_index(self.tag_genome_df, self.genre_df)
+            df = merge_df_on_index(tg_df, self.movie_df)[['title']]
+            id_series = tg_df.index
+
+            tag_vec_df = self.tag_genome_df.replace(np.NaN, 0).mul(tag_weight)
+            genre_vec_df = self.genre_df.replace(np.NaN, 0).mul(genre_weight)
+            vector_df = merge_df_on_index(tag_vec_df, genre_vec_df)
+
+            x_array = vector_df.loc[movie_id].to_numpy()
+            y_array = vector_df.to_numpy()
+
+            t = cosine_similarity(x_array.reshape(1, -1), y_array)
+            cosine_df = pd.DataFrame(
+                {'movieId': id_series, 'cos': t[0]}
+            ).set_index('movieId')
+
+            movie_cos_df = merge_df_on_index(df, cosine_df)
+            movie_cos_df = movie_cos_df.drop(index=movie_id).sort_values(
+                'cos', ascending=False)
+            return movie_cos_df.head(top_n).to_dict(orient='index')
+        else:
+            return []
