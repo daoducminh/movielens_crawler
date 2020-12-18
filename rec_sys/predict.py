18598ae0ec1c6c975b5ccc6248f28463b1a13b66
Minh Dao <daoducminh1997@gmail.com>
2020-12-18 20:28:12 +0700

    Update

--- rec_sys/predict.py
+++ rec_sys/predict.py
@@ -13,33 +13,39 @@ def user_based_cf(dataset, user_ratings, top_n=10, k_neighbor=20):
     else:
         recommender = UserBasedCFRecommender(dataset)
         return recommender.top_movies(user_ratings, top_n, k_neighbor)
-        # return recommender.mae(user_ratings, k_neighbor)
+
+
+def user_based_cf_mae(dataset, user_ratings, top_n=10, k_neighbor=20):
+    recommender = UserBasedCFRecommender(dataset)
+    return recommender.mae(user_ratings, k_neighbor)
 
 
 def content_based_on_tags(dataset, movie_id):
     recommender = ContentBasedRecommender(dataset)
-    # return recommender.top_cos_genome_tag(movie_id, top_n=10)
     return recommender.top_cos_tag(movie_id, top_n=10)
 
 
+def content_based_on_tag_genome(dataset, movie_id):
+
+    recommender = ContentBasedRecommender(dataset)
+    return recommender.top_cos_genome_tag(movie_id, top_n=10)
+
+
 if __name__ == '__main__':
-    # data = Dataset(DATESET_SMALL, 'csv', ',')
-    data = Dataset(DATASET_1M, 'csv', ',')
+    data = Dataset(DATESET_SMALL, 'csv', ',')
 
     # Content based
-    # print(content_based_on_tags(data, movie_id=246))
+    print('Content based on tags')
+    print(content_based_on_tags(data, movie_id=246))
+    print('\nContent based on tag genome')
+    print(content_based_on_tag_genome(data, movie_id=246))
 
     # User based
-    # rate = {}
-    # ids = (1, 3, 6, 47, 50, 70, 101, 110, 151, 157, 163, 216, 223, 231, 235)
-    # sc = (4.0, 4.0, 4.0, 5.0, 5.0, 3.0, 5.0,
-    #       5.0, 4.0, 4.0, 4.5, 4.5, 3.5, 5.0, 4.5)
-    # for i in range(len(ids)):
-    #     rate[ids[i]] = sc[i]
-    # print(user_based_cf(data, rate, top_n=10, k_neighbor=50))
-
     movies = list(data.load_df('movies').loc[:, 'movieId'])
     rate = {}
     for i in range(50):
         rate[movies[i]] = SCORES[random.randrange(0, len(SCORES), 1)]
+    print('\nUser based CF')
     print(user_based_cf(data, rate, top_n=10, k_neighbor=50))
+    print('\nMAE evaluation on User based CF')
+    print(user_based_cf_mae(data, rate, top_n=10, k_neighbor=50))
